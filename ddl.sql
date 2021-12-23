ed5c0e26472b8318da24bbcbcd1f2d75efef498a
franciscoparente <francisco.parente@mercadolivre.com>
2021-12-23 11:41:02 -0300

    initial

---
+++ ddl.sql
@@ -0,0 +1,5 @@
+-- debt.testes definition
+
+CREATE TABLE `testes` (
+  `title` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	db *sql.DB
+)
+
+func main() {
+	initdb()
+	exampleWithoutLock()
+	exampleLock()
+}
+
+func exampleWithoutLock() {
+	tx, errTx := db.Begin()
+	if errTx != nil {
+		log.Print("err tx ", errTx)
+	}
+
+	// update with tx
+	qryu := `UPDATE testes
+	SET title='1';`
+
+	_, errUpdate := tx.Exec(qryu)
+	if errUpdate != nil {
+		log.Print("err exec ", errUpdate)
+	}
+
+	// select without tx
+	qry := `
+	SELECT
+		title
+	FROM testes;`
+
+	rows, errQuery := db.Query(qry)
+	if errQuery != nil {
+		log.Print("err query ", errQuery)
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		column := ""
+
+		errScan := rows.Scan(&column)
+		if errScan != nil {
+			log.Print("err query ", column)
+		}
+
+		log.Print("result column ", column)
+	}
+
+	tx.Rollback()
+}
+
+func exampleLock() {
+	tx, errTx := db.Begin()
+	if errTx != nil {
+		log.Print("err tx ", errTx)
+	}
+
+	// update with tx
+	qryu := `UPDATE testes
+	SET title='1';`
+
+	_, errUpdate := tx.Exec(qryu)
+	if errUpdate != nil {
+		log.Print("err exec ", errUpdate)
+	}
+
+	// insert without tx - lock
+	qryi := `INSERT INTO testes
+	(title) 
+	VALUES ('TESTE');`
+
+	_, errInsert := db.Exec(qryi)
+	if errInsert != nil {
+		log.Print("err exec ", errInsert)
+	}
+
+	tx.Rollback()
+}
+
+func initdb() {
+	dba, err := sql.Open("mysql", "root:r00t@/debt")
+	if err != nil {
+		log.Panic("error open connection database ", err)
+	}
+
+	dba.SetMaxOpenConns(5)
+	dba.SetMaxIdleConns(2)
+	dba.SetConnMaxLifetime(time.Minute * 5)
+
+	if err = dba.Ping(); err != nil {
+		log.Panic("error ping connection database ", err)
+	}
+
+	db = dba
+}
