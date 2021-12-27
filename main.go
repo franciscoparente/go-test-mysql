578ad0863623b9c5e361608d0fe694a634de2d31
franciscoparente <francisco.parente@mercadolivre.com>
2021-12-27 10:57:59 -0300

    examples

--- main.go
+++ main.go
@@ -14,8 +14,97 @@ var (
 
 func main() {
 	initdb()
-	exampleWithoutLock()
-	exampleLock()
+	exampleSelectUpdate()
+	//exampleUpdateSelect()
+	// exampleWithoutLock()
+	// exampleLock()
+}
+
+func exampleSelectUpdate() {
+	tx, errTx := db.Begin()
+	if errTx != nil {
+		log.Print("err tx ", errTx)
+	}
+
+	tx2, errTx2 := db.Begin()
+	if errTx2 != nil {
+		log.Print("err tx ", errTx2)
+	}
+
+	// select without tx
+	qry := `
+		SELECT
+			title
+		FROM testes;`
+
+	rows, errQuery := tx.Query(qry)
+	if errQuery != nil {
+		log.Print("err query ", errQuery)
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		column := ""
+
+		errScan := rows.Scan(&column)
+		if errScan != nil {
+			log.Print("err query ", column)
+		}
+
+		log.Print("result column ", column)
+	}
+
+	// update with tx
+	qryu := `UPDATE testes
+		SET title='2';`
+
+	_, errUpdate := tx2.Exec(qryu)
+	if errUpdate != nil {
+		log.Print("err exec ", errUpdate)
+	}
+
+	tx.Rollback()
+}
+
+func exampleUpdateSelect() {
+	tx, errTx := db.Begin()
+	if errTx != nil {
+		log.Print("err tx ", errTx)
+	}
+
+	// update with tx
+	qryu := `UPDATE testes
+	SET title='1';`
+
+	_, errUpdate := tx.Exec(qryu)
+	if errUpdate != nil {
+		log.Print("err exec ", errUpdate)
+	}
+
+	// select without tx
+	qry := `
+	SELECT
+		title
+	FROM testes;`
+
+	rows, errQuery := tx.Query(qry)
+	if errQuery != nil {
+		log.Print("err query ", errQuery)
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		column := ""
+
+		errScan := rows.Scan(&column)
+		if errScan != nil {
+			log.Print("err query ", column)
+		}
+
+		log.Print("result column ", column)
+	}
+
+	tx.Rollback()
 }
 
 func exampleWithoutLock() {
